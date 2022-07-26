1440b0b7fd0d48d142f18525586bfdbceac83044
Katbglitch <62664198+Katbglitch@users.noreply.github.com>
2022-07-26 13:10:40 +0100

    Update DOA.py

--- DOA.py
+++ DOA.py
@@ -7,10 +7,10 @@ dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
  
 if dev:
     Mic_tuning = Tuning(dev)
-    print Mic_tuning.direction
     while True:
         try:
-            print Mic_tuning.direction
+            print(Mic_tuning.direction)
+            print(Mic_tuning.is_voice())
             time.sleep(1)
         except KeyboardInterrupt:
             break
